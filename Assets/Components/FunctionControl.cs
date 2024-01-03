c9d0760ac919c4792feacd819677d0d3d4fbec64
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-01-03 19:40:36 +0100

    ajout ptite bulle sur l'objet prefab + cf comm

--- Assets/Components/FunctionControl.cs
+++ Assets/Components/FunctionControl.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class FunctionControl : ControlElement {
 	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
-	public string functionName;
+	public string functionName;    // No name variable, we should directly access the name written on the
+								   // UI element if I understand ECS right i probably don't though
 }
